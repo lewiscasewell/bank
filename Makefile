5856ba64b15325ff1239a473e675ef09c60cf1e0
Lewis Casewell <lewiscasewell@hotmail.co.uk>
2023-10-08 18:52:41 +0100

    added accounts, entry and transfers queries using sqlc

--- Makefile
+++ Makefile
@@ -22,4 +22,7 @@ migratedown:
 sqlc:
 	sqlc generate
 
-.PHONY: createdb dropdb migrateup migratedown
+test:
+	go test -v -cover ./...
+
+.PHONY: createdb dropdb migrateup migratedown sqlc test
---
+++ db/sqlc/main_test.go
@@ -0,0 +1,28 @@
+package db
+
+import (
+	"context"
+	"log"
+	"os"
+	"testing"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+const (
+	dbDriver = "postgres"
+	dbSource = "postgresql://postgres:postgres@localhost:5432/bank?sslmode=disable"
+)
+
+var testQueries *Queries
+
+func TestMain(m *testing.M) {
+	conn, err := pgxpool.New(context.Background(), dbSource)
+	if err != nil {
+		log.Fatalf("cannot connect to db: %v", err)
+	}
+
+	testQueries = New(conn)
+
+	os.Exit(m.Run())
+}
